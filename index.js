55a6079b88f3ec1a303754c20472457ac8308d31
Rakibul2580 <rh565153@gmail.com>
2022-10-24 18:36:29 +0300

    setup complet

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 const port = 5000;
+const name = require("./data/items-name.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(name);
 });
 app.get("/n", (req, res) => {
-  res.send("Hello World!!!!");
+  res.send({ name: "Rakibul Hasan" });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// git init
+// git add
+// git commit -m ""
+// git push -u origin main
